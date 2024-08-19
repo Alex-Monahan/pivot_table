e6547795fb872785c1ba50ffdcf9f47e1f1c3c85
Alex-Monahan <alex@motherduck.com>
2024-08-19 08:21:40 -0700

    change name to pivot_table

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# Quack
+# Pivot_table
 
 This repository is based on https://github.com/duckdb/extension-template, check it out if you want to build and ship your own DuckDB extension.
 
 ---
 
-This extension, Quack, allow you to ... <extension_goal>.
+This extension, Pivot_table, allow you to ... <extension_goal>.
 
 
 ## Building
@@ -26,23 +26,23 @@ The main binaries that will be built are:
 ```sh
 ./build/release/duckdb
 ./build/release/test/unittest
-./build/release/extension/quack/quack.duckdb_extension
+./build/release/extension/pivot_table/pivot_table.duckdb_extension
 ```
 - `duckdb` is the binary for the duckdb shell with the extension code automatically loaded.
 - `unittest` is the test runner of duckdb. Again, the extension is already linked into the binary.
-- `quack.duckdb_extension` is the loadable binary as it would be distributed.
+- `pivot_table.duckdb_extension` is the loadable binary as it would be distributed.
 
 ## Running the extension
 To run the extension code, simply start the shell with `./build/release/duckdb`.
 
-Now we can use the features from the extension directly in DuckDB. The template contains a single scalar function `quack()` that takes a string arguments and returns a string:
+Now we can use the features from the extension directly in DuckDB. The template contains a single scalar function `pivot_table()` that takes a string arguments and returns a string:
 ```
-D select quack('Jane') as result;
+D select pivot_table('Jane') as result;
 ┌───────────────┐
 │    result     │
 │    varchar    │
 ├───────────────┤
-│ Quack Jane 🐥 │
+│ Pivot_table Jane 🐥 │
 └───────────────┘
 ```
 
@@ -81,6 +81,6 @@ DuckDB. To specify a specific version, you can pass the version instead.
 
 After running these steps, you can install and load your extension using the regular INSTALL/LOAD commands in DuckDB:
 ```sql
-INSTALL quack
-LOAD quack
+INSTALL pivot_table
+LOAD pivot_table
 ```
---
+++ src/pivot_table_extension.cpp
@@ -0,0 +1,78 @@
+#define DUCKDB_EXTENSION_MAIN
+
+#include "pivot_table_extension.hpp"
+#include "duckdb.hpp"
+#include "duckdb/common/exception.hpp"
+#include "duckdb/common/string_util.hpp"
+#include "duckdb/function/scalar_function.hpp"
+#include "duckdb/main/extension_util.hpp"
+#include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
+
+// OpenSSL linked through vcpkg
+#include <openssl/opensslv.h>
+
+namespace duckdb {
+
+inline void PivotTableScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
+    auto &name_vector = args.data[0];
+    UnaryExecutor::Execute<string_t, string_t>(
+	    name_vector, result, args.size(),
+	    [&](string_t name) {
+			return StringVector::AddString(result, "PivotTable "+name.GetString()+" 🐥");;
+        });
+}
+
+inline void PivotTableOpenSSLVersionScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
+    auto &name_vector = args.data[0];
+    UnaryExecutor::Execute<string_t, string_t>(
+	    name_vector, result, args.size(),
+	    [&](string_t name) {
+			return StringVector::AddString(result, "PivotTable " + name.GetString() +
+                                                     ", my linked OpenSSL version is " +
+                                                     OPENSSL_VERSION_TEXT );;
+        });
+}
+
+static void LoadInternal(DatabaseInstance &instance) {
+    // Register a scalar function
+    auto pivot_table_scalar_function = ScalarFunction("pivot_table", {LogicalType::VARCHAR}, LogicalType::VARCHAR, PivotTableScalarFun);
+    ExtensionUtil::RegisterFunction(instance, pivot_table_scalar_function);
+
+    // Register another scalar function
+    auto pivot_table_openssl_version_scalar_function = ScalarFunction("pivot_table_openssl_version", {LogicalType::VARCHAR},
+                                                LogicalType::VARCHAR, PivotTableOpenSSLVersionScalarFun);
+    ExtensionUtil::RegisterFunction(instance, pivot_table_openssl_version_scalar_function);
+}
+
+void PivotTableExtension::Load(DuckDB &db) {
+	LoadInternal(*db.instance);
+}
+std::string PivotTableExtension::Name() {
+	return "pivot_table";
+}
+
+std::string PivotTableExtension::Version() const {
+#ifdef EXT_VERSION_PIVOT_TABLE
+	return EXT_VERSION_PIVOT_TABLE;
+#else
+	return "";
+#endif
+}
+
+} // namespace duckdb
+
+extern "C" {
+
+DUCKDB_EXTENSION_API void pivot_table_init(duckdb::DatabaseInstance &db) {
+    duckdb::DuckDB db_wrapper(db);
+    db_wrapper.LoadExtension<duckdb::PivotTableExtension>();
+}
+
+DUCKDB_EXTENSION_API const char *pivot_table_version() {
+	return duckdb::DuckDB::LibraryVersion();
+}
+}
+
+#ifndef DUCKDB_EXTENSION_MAIN
+#error DUCKDB_EXTENSION_MAIN not defined
+#endif
